3170730698a68e5b17291c8fe6832d839b1eb720
kurtgodel95 <pratiks256@gmail.com>
2021-10-24 17:54:33 -0500

    Integrate with crawler

--- project/pagerank/pagerank.py
+++ project/pagerank/pagerank.py
@@ -1,7 +1,9 @@
-def pagerank(corpus_size, graph={}, d=0.85):
+import json
+
+def pagerank(output_file_name, corpus_size, graph={}, d=0.85):
     n = len(graph)
-    iter1 = [1.0/corpus_size]*corpus_size
-    iter2 = iter1[:]
+    iter1 = {k: 1.0/corpus_size for k in graph}
+    iter2 = iter1.copy()
     sinc_nodes = [i for i in graph if len(graph[i])==0]
     while True:
         sinc_nodes_contrib = sum([iter1[i]/corpus_size for i in sinc_nodes ])
@@ -9,17 +11,12 @@ def pagerank(corpus_size, graph={}, d=0.85):
             in_neighbors = [x for x in graph if s in graph[x]]
             iter2[s] = (1-d)/corpus_size + \
                 d*(sum([iter1[i]/len(graph[i]) for i in in_neighbors]) + sinc_nodes_contrib)
-        if sum([abs(iter2[i] - iter1[i]) for i in range(len(iter1))]) < 1e-10:
+        if sum([abs(iter2[i] - iter1[i]) for i in iter1]) < 1e-10:
             break
-        iter1, iter2 = iter2, iter1
-    return iter1
+        iter1 = iter2.copy()
+    with open(output_file_name, 'w') as fp:
+        json.dump(iter1, fp)
 
-
-graph = {0:[1], 1:[2], 2:[0]}
-
-pagerank(3, graph, 0.85)
-
-
-
-
-
+#with open('../crawler/crawler_result.json', 'r') as fp:
+#    graph = json.load(fp)
+#pagerank("pagerank_result.json", len(graph), graph, 0.85)
