1d917416ece430e94c53516751d7c50dafce5a84
aarontcopal2 <aarontcopal2@gmail.com>
2021-10-24 16:03:18 -0500

    updated crawler.py to ouput the key-val results in a JSON file

--- project/crawler/crawler.py
+++ project/crawler/crawler.py
@@ -4,68 +4,86 @@ import requests
 import validators
 import ssl
 import urllib3
+import json
 
-# set initial data-structures
-depth, max_depth =1,3
-urls = set() # we use set to maintain distinct urls
-new_urls = set() # we use set to maintain distinct urls
-completed_urls = []
-failed_urls = set()
+class Crawler:
+    # default constructor
+    def __init__(self, user_inputs):
+        # set initial data-structures
+        self.user_inputs = user_inputs
+        self.depth, self.max_depth =1,2
+        self.urls = set() # we use set to maintain distinct urls
+        self.new_urls = set() # we use set to maintain distinct urls
+        self.completed_urls = []
+        self.failed_urls = set()
+        self.graph = {}
 
-# get the list of urls from user
-user_inputs = ["https://www.rice.edu/"]
 
-# append the urls to urls set
-for url in user_inputs:
-    urls.add(url)
+    def crawl(self):
+        # append the urls to urls set
+        for url in self.user_inputs:
+            self.urls.add(url)
 
-while depth <= max_depth:
-    while (len(urls) != 0): 
-        url = urls.pop()
-        
-        # get url's html content
-        try:
-            response = requests.get(url)
-        except (requests.exceptions.InvalidSchema, ssl.SSLError, urllib3.exceptions.MaxRetryError,
-                 requests.exceptions.SSLError, requests.exceptions.ConnectionError) as e:
-            failed_urls.add(url)
-            continue
+        while self.depth <= self.max_depth:
+            while (len(self.urls) != 0): 
+                url = self.urls.pop()
+                
+                # get url's html content
+                try:
+                    response = requests.get(url)
+                except (requests.exceptions.InvalidSchema, ssl.SSLError, urllib3.exceptions.MaxRetryError,
+                        requests.exceptions.SSLError, requests.exceptions.ConnectionError) as e:
+                    self.failed_urls.add(url)
+                    continue
 
-        # check if response is valid
-        if response.status_code != 200:
-            failed_urls.add(url)
-            continue
+                # check if response is valid
+                if response.status_code != 200:
+                    self.failed_urls.add(url)
+                    continue
 
-        # use beautiful soup object to extract all hyperlinks
-        soup = BeautifulSoup(response.text, 'html.parser')
+                # use beautiful soup object to extract all hyperlinks
+                soup = BeautifulSoup(response.text, 'html.parser')
 
-        for link in soup.find_all('a'):
-            extracted_url = link.get('href')
+                for link in soup.find_all('a'):
+                    extracted_url = link.get('href')
 
-            # In some cases, the extracted_url may be None, if so these invalid links shouldnt be processed
-            if not isinstance(extracted_url, str):
-                continue
-            
-            is_valid_url = validators.url(extracted_url)
+                    # In some cases, the extracted_url may be None, if so these invalid links shouldnt be processed
+                    if not isinstance(extracted_url, str):
+                        continue
+                    
+                    is_valid_url = validators.url(extracted_url)
 
-            # if extracted links is valid and not in completed_urls and not equal to url, add to new_urls
-            if (is_valid_url==True and extracted_url not in completed_urls 
-                and extracted_url not in failed_urls and extracted_url != url):
-                new_urls.add(extracted_url)
+                    # if extracted links is valid and not in completed_urls and not equal to url, add to new_urls
+                    if (is_valid_url==True  
+                        and extracted_url not in self.failed_urls and extracted_url != url):
+                        self.new_urls.add(extracted_url)
 
-        completed_urls.append(url)
+                self.completed_urls.append(url)
+                self.graph[url] = self.new_urls
 
-    print("depth: " + str(depth) + ":: new_urls: " + str(len(new_urls)))
-    urls = new_urls.copy()
-    new_urls.clear()
-    depth += 1
+            # print("depth: " + str(self.depth) + ":: new_urls: " + str(len(self.new_urls)))
+            self.new_urls = set([page for page in self.new_urls if page not in self.completed_urls])
+            self.urls = self.new_urls.copy()
+            self.new_urls.clear()
+            self.depth += 1
 
-print("\ncompleted urls: ")
-# print list of completed urls
-for url in completed_urls:
-    print(url)
 
-print("\nfailed urls: ")
-# print list of failed urls
-for url in failed_urls:
-    print(url)
+        # print("\ncompleted urls: ")
+        # # print list of completed urls
+        # for url in self.completed_urls:
+        #     print(url)
+
+        # print("\nfailed urls: ")
+        # # print list of failed urls
+        # for url in self.failed_urls:
+        #     print(url)
+
+        # print("\ngraph output: ")
+        # print(self.graph)
+        self.graph = {k: list(v) for k, v in self.graph.items()}
+        with open('crawler_result.json', 'w') as fp:
+            json.dump(self.graph, fp)
+
+
+c= Crawler(["https://www.rice.edu/"])
+c.crawl()
