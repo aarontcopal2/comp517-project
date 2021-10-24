1679cd96d4a924e8f98e4e17c574ce10890d921d
Aniket Gattani <aniketgattani@gmail.com>
2021-10-24 18:09:53 -0500

    added crawling functionality

--- project/peer-discovery/client.py
+++ project/peer-discovery/client.py
@@ -4,12 +4,15 @@ import time
 from node import Node
 from nodeconnection import NodeConnection
 import json
+import os
 
 class PeerClient(threading.Thread):
 	msg = {"message":"", "value":""}
 	send_all_nodes_msg = "send_all_nodes_data"
 	get_all_nodes_msg = "get_all_nodes_data"
+	crawl_msg = "crawl"
 	master_node_id = 1
+	output_dir = "/output"
 
 	def __init__(self, host_ip, host_port, host_id, debug=False):
 		threading.Thread.__init__(self)
@@ -31,11 +34,33 @@ class PeerClient(threading.Thread):
 	def connect_with_node(self, node_ip, node_port):
 		self.node.connect_with_node(node_ip, node_port)
 	
+	def connect_with_nodes(self, data):
+		node_list = eval(data['value'])
+		for node in node_list:
+			self.connect_with_node(node['host'], node['port'])
+	
 	def connect_with_master_node(self, node_ip, node_port):
 		self.connect_with_node(node_ip, node_port)
 		self.node.print_connections()
 		time.sleep(5)
-		self.get_all_nodes_from_master()
+		self.get_all_nodes_fromtpu_master()
+
+	# crawl the input  
+	def crawl(self, data):
+		lines = []
+		op_file = out_dir + "/" + self.id + "./done"
+		os.remove(op_file)
+		with open(data['value'], 'r') as f:
+    		lines = f.readlines()
+    	tot_lines = len(lines)
+    	n = len(get_all_connected_nodes())
+    	webpages=[]
+    	for i in range((tot_lines/n)*self.id , min((tot_lines/n)*(self.id+1),tot_lines)):
+    		webpages.append(tot_lines[i])
+    	
+    	c = Crawler(webpages, output_dir + "/" + self.id + "_crawl_output.json")
+    	c.crawl()
+    	f =  open(op_file, "w+")
 
 	def send_to_node(self, n, data):
 		n=str(n)
@@ -43,23 +68,28 @@ class PeerClient(threading.Thread):
 
 	def send_to_all_nodes(self, data, exclude=[]):
 		self.node.send_to_nodes(json.dumps(data), exclude)
+
+	def get_all_connected_nodes(self):
+		s = self.node.nodes_inbound:
+		s += sel0f.node.nodes_outbound 
+		return s
 	
+	def send_info_abt_all_nodes(self, to_node, data):
+		s = self.get_all_connected_nodes()
+		s = [{"host": node.connected_node_host, "port": node.connected_node_port} for node in s ]
+		msg = PeerClient.msg
+		msg['message'] = PeerClient.get_all_nodes_msg 
+		msg['value'] = str(s)
+		self.send_to_node(to_node.id, json.dumps(msg))
+			
 	def check_node_message(self, from_node, data):
 		data = json.loads(data)
 		if data['message'] == PeerClient.send_all_nodes_msg :
-			s = self.node.nodes_inbound
-			s += self.node.nodes_outbound
-			print(s)
-			s = [{"host": node.connected_node_host, "port": node.connected_node_port} for node in s ]
-			msg = PeerClient.msg
-			msg['message'] = PeerClient.get_all_nodes_msg 
-			msg['value'] = str(s)
-			self.send_to_node(from_node.id, json.dumps(msg))
+			self.send_info_abt_all_nodes(from_node, data)
 		elif data['message'] == PeerClient.get_all_nodes_msg:
-			node_list = eval(data['value'])
-			for node in node_list:
-				self.connect_with_node(node['host'], node['port'])
-
+			self.connect_with_nodes(data)
+		elif data['message'] == PeerClient.crawl_msg:
+		
 	def get_all_nodes_from_master(self):
 		msg = {}
 		msg['message'] = PeerClient.send_all_nodes_msg
--- project/peer-discovery/master_peer.py
+++ project/peer-discovery/master_peer.py
@@ -10,6 +10,7 @@
 import sys
 import os
 import time
+import re
 sys.path.insert(0, '..') # Import the files where the modules are located
 
 from client import PeerClient
@@ -17,6 +18,39 @@ from client import PeerClient
 def stop_node(node):
 	if node is not None:
 		node.stop()
+
+def send_crawl():
+	msg = PeerClient.msg
+	msg['message'] = "crawl"
+	msg['value'] = s 
+	master_node.send_to_nodes(json.dumps(msg))
+	done_nodes = []
+	tot_nodes = master_node.get_all_connected_nodes()
+	while len(done_nodes) != len(tot_nodes):
+		time.sleep(5)
+		for node in tot_nodes:
+			if node not in done_nodes and os.path.isfile(PeerClient.output_dir + "/" + node.id + ".done"):
+				done_nodes.append(node)
+				print(node.id + " done")
+	crawl_output = {}
+	for node in done_nodes:
+		with open(PeerClient.output_dir + "/" + node.id + "_crawl_output.json") as f:
+			 f_output = json.loads(f)
+			 for link in f_output:
+			 	if link in crawl_output:
+			 		crawl_output[link].extend(link)
+	with open(PeerClient.output_dir + "/" + "_crawl_ouput.json", "w+") as f:
+		json.dump(crawl_output, f)
+	return crawl_output
+
+# def send_pagerank():
+# 	msg = PeerClient.msg
+# 	msg['message'] = "pagerank"
+# 	msg['value'] = s 
+# 	master_node.send_to_nodes(json.dumps(msg))
+
+	
+
 
 def main():
 	master_node = None
@@ -36,11 +70,8 @@ def main():
 			if s == 'q' :
 				stop_node(master_node)
 			os._exit(1)
-			# elif len(s)>1 and s[0] == 's' :
-			# 	msg = PeerClient.msg
-			# 	msg['message'] = "from_master"
-			# 	msg['value'] = s[2:] 
-			# 	self.send_to_nodes(json.dumps(x))	
+			elif re.search(".txt$", s) is not None:
+				
 
 	except Exception as e:
 		raise e
